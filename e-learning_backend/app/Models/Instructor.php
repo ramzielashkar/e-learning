22f1d3c3216bf14be66aff63693ca0915ec26bc5
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-08 13:47:41 +0300

    Backend: Adding Instructor Model

---
+++ e-learning_backend/app/Models/Instructor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Instructor extends Model
+{
+    use HasFactory;
+    protected $connection = 'mongodb';
+
+    protected $fillable = [
+        'name',
+    ];
+
+
+}
