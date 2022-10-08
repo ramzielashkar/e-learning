65f9589ae4ad9fc2309b465cb1850551de74d1b0
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-08 15:43:53 +0300

    Backend: Enabling Instructor Middleware

--- e-learning_backend/app/Http/Kernel.php
+++ e-learning_backend/app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role.admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'role.instructor' => \App\Http\Middleware\InstructorMiddleware::class,
 
     ];
 }
