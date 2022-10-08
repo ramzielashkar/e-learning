65f731bb4d71c32ba54d3e0fac90f23575fb8919
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-08 14:49:04 +0300

    Backend: Adding Get Courses Route

--- e-learning_backend/routes/api.php
+++ e-learning_backend/routes/api.php
@@ -10,7 +10,7 @@ Route::group(["prefix"=> "v0.1"], function(){
     Route::post("/addUser", [AdminController::class, "addUser"])->name("register-user");
     Route::post("/addCourse", [AdminController::class, "addCourse"])->name("add-course");
     Route::get("/getUsers/{type}", [AdminController::class, "getUsers"])->name("get-users");
-
+    Route::get("/getCourses", [AdminController::class, "getCourses"])->name("get-courses");
 });
   Route::post("/login", [AuthController::class, "login"])->name("login-user");
 
