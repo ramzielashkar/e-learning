d7148a00ee2219286e102da74c94d87c8638b825
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-08 14:45:52 +0300

    Backend: Adding Add Course Route

--- e-learning_backend/routes/api.php
+++ e-learning_backend/routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\AdminController;
 Route::group(["prefix"=> "v0.1"], function(){
   Route::group(["middleware" => "role.admin"], function(){
     Route::post("/addUser", [AdminController::class, "addUser"])->name("register-user");
+    Route::post("/addCourse", [AdminController::class, "addCourse"])->name("add-course");
     Route::get("/getUsers/{type}", [AdminController::class, "getUsers"])->name("get-users");
 
 });
