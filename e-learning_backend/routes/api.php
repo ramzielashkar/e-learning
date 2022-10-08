57a0f6d77e0570014681c120ea3aff545ff5af9c
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-08 14:41:24 +0300

    Backend: Adding Admin Middleware and some Routes

--- e-learning_backend/routes/api.php
+++ e-learning_backend/routes/api.php
@@ -3,5 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 
-Route::post("/register", [AuthController::class, "register"])->name("register-user");
+Route::group(["prefix"=> "v0.1"], function(){
+  Route::group(["middleware" => "role.admin"], function(){
+    Route::post("/addUser", [AdminController::class, "addUser"])->name("register-user");
+    Route::get("/getUsers/{type}", [AdminController::class, "getUsers"])->name("get-users");
+
+});
+  Route::post("/login", [AuthController::class, "login"])->name("login-user");
+
+  Route::get("/not_found", [AuthController::class, "notFound"])->name("not-found");
+
+});
