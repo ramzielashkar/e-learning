db494c7f64c79152a8d67511dce88e2a26936cbe
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-08 15:32:28 +0300

    Backend: Adding Authentication Middleware

--- e-learning_backend/routes/api.php
+++ e-learning_backend/routes/api.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 
 Route::group(["prefix"=> "v0.1"], function(){
-  Route::group(["middleware" => "role.admin"], function(){
-    Route::post("/addUser", [AdminController::class, "addUser"])->name("register-user");
-    Route::post("/addCourse", [AdminController::class, "addCourse"])->name("add-course");
-    Route::get("/getUsers/{type}", [AdminController::class, "getUsers"])->name("get-users");
-    Route::get("/getCourses", [AdminController::class, "getCourses"])->name("get-courses");
-    Route::post("/assignCourse", [AdminController::class, "assignCourse"])->name("assign-course");
+  Route::group(["middleware" => "auth:api"], function(){
+    Route::group(["middleware" => "role.admin"], function(){
+      Route::post("/addUser", [AdminController::class, "addUser"])->name("register-user");
+      Route::post("/addCourse", [AdminController::class, "addCourse"])->name("add-course");
+      Route::get("/getUsers/{type}", [AdminController::class, "getUsers"])->name("get-users");
+      Route::get("/getCourses", [AdminController::class, "getCourses"])->name("get-courses");
+      Route::post("/assignCourse", [AdminController::class, "assignCourse"])->name("assign-course");
 
+  });
 });
   Route::post("/login", [AuthController::class, "login"])->name("login-user");
 
