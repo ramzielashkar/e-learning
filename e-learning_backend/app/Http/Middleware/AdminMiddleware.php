758fe7eca00b9356d1934e00b4407bc6ae2a65f6
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-08 15:44:13 +0300

    Backend: Updating Admin Middleware

--- e-learning_backend/app/Http/Middleware/AdminMiddleware.php
+++ e-learning_backend/app/Http/Middleware/AdminMiddleware.php
@@ -8,13 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
+
     public function handle(Request $request, Closure $next)
     {
         $user = Auth::user();
