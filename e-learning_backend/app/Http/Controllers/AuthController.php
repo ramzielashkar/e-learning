8ccaa07aedf5f08f42ad7d598aeac1b25d265569
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-08 14:38:54 +0300

    Backend: Updating Authentication Controller

--- e-learning_backend/app/Http/Controllers/AuthController.php
+++ e-learning_backend/app/Http/Controllers/AuthController.php
@@ -27,37 +27,6 @@ class AuthController extends Controller
         ]);
     }
 
-    public function register(Request $request)
-      {
-        $request->validate([
-       'name' => 'required|string|max:255',
-       'email' => 'required|string|email|max:255|unique:users',
-       'password' => 'required|string|min:6',
-       ]);
-       $user = User::create([
-           'name' => $request->name,
-           'email' => $request->email,
-           'password' => Hash::make($request->password),
-           'type'=>$request->type
-       ]);
-       if($request->type == 'student'){
-         $student = Student::create([
-           'name' => $request->name,
-         ]);
-       }
-       else if($request->type == 'instructor'){
-         $instructor = Instructor::create([
-           'name' => $request->name,
-         ]);
-       }
-       $token = Auth::login($user);
-       $user->token = $token;
-
-       return response()->json([
-         'user'=>$user,
-       ]);
-      }
-
 
     public function me()
     {
@@ -76,12 +45,9 @@ class AuthController extends Controller
         return $this->respondWithToken(auth()->refresh());
     }
 
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
-    }
+    function notFound(){
+          return response()->json([
+              "status" => "Unauthorized",
+          ]);
+      }
 }
