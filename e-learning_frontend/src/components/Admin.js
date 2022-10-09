19379834cff9d5d1512c8053516d84f6fa91e1db
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 18:16:51 +0300

    Updating Admin page

--- e-learning_frontend/src/components/Admin.js
+++ e-learning_frontend/src/components/Admin.js
@@ -1,9 +1,12 @@
 import AdminSidebar from "./AdminSidebar";
+import { Outlet } from "react-router-dom";
 
 const Admin = () =>{
     return (
+      <div className="admin-page flex ">
       <AdminSidebar />
-      
+      <Outlet />
+      </div>
     );
 };
 
