34f9011710fff42b2d73f646a5908cd8e7ffe384
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 15:00:26 +0300

    Updating Login Form Component

--- e-learning_frontend/src/components/Login.js
+++ e-learning_frontend/src/components/Login.js
@@ -1,29 +1,46 @@
 import React, { useState } from "react";
+const Login = ({onAdd}) =>{
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const Login = () =>{
+  const onSubmit = (e) => {
+  e.preventDefault();
+  if (!email) {
+    console.log('error');
+    return;
+  }
+    //console.log(email, password);
+  onAdd({ email, password });
+
+  setEmail("");
+  setPassword("");
+};
+
   return(
     <section className = 'login-section flex column'>
-      <form className = "login-form">
-      <div className="form-control flex column">
-      <label>Email</label>
-      <input
-        type="email"
-        placeholder="Enter your Email"
-        value=''
-        //onChange={(e) => setText(e.target.value)}
-      />
-    </div>
+      <form className = "login-form flex column" onSubmit={onSubmit}>
+        <h2>Start a New Journey !</h2>
+        <div className="form-control flex column">
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Enter your Email"
+            onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-    <div className="form-control flex column">
-    <label>Password</label>
-    <input
-      type="password"
-      placeholder="Enter password"
-      value=''
-      //onChange={(e) => setText(e.target.value)}
-    />
-  </div>
-      </form>
+        <div className="form-control flex column">
+          <label>Password</label>
+          <input
+            type="password"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          </div>
+        <input type={"submit"} value="Login" className="btn btn-purple" />
+    </form>
     </section>
   );
 
