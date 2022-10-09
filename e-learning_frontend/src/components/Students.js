d7e786a042be213f84ddf7ea9fb0a586789dfd0f
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 23:08:51 +0300

    Adding Students Page Component

--- e-learning_frontend/src/components/Students.js
+++ e-learning_frontend/src/components/Students.js
@@ -1,6 +1,20 @@
 import Button from './Button';
+import UserCard from './UserCard';
+import { useState, useEffect } from "react";
+import AddStudent from './AddStudent';
+
 const Students = () => {
+  const showAddPopup = () => {
+    setShowAddStudent(true);
+  };
+  const closeAddPopup = () => {
+    setShowAddStudent(false);
+  };
+
+  const [showAddStudent, setShowAddStudent] = useState(false);
+
   return(
+    <>
     <section className = 'section flex column'>
       <h1 className = 'title'>Students</h1>
       <div className='add-course-btn flex'>
@@ -8,12 +22,21 @@ const Students = () => {
         text = {'Add Student'}
         color = {'btn-purple'}
         onClick = {() => {
+          showAddPopup();
         }}
         />
       </div>
       <div className = 'all-students flex'>
+      <UserCard/>
       </div>
     </section>
+    <AddStudent
+    open = {showAddStudent}
+    onClose = {() => {
+      closeAddPopup();
+    }}
+    />
+    </>
   );
 };
 export default Students;
