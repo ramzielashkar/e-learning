55bd9504553e0b5e8f47c92a1fd8073d0759b227
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 21:00:53 +0300

    Updating All Courses Component

--- e-learning_frontend/src/components/AdminCourses.js
+++ e-learning_frontend/src/components/AdminCourses.js
@@ -1,18 +1,42 @@
 import Course from './Course';
+import AssignCourses from './AssignCourses';
+import { useState, useEffect } from "react";
+
 const AdminCourses = () =>{
+  const showAssignPopup = () => {
+    setShowAssignCourse(true);
+  };
+const closeAssignPopup = () => {
+  setShowAssignCourse(false);
+};
+
+  const [coursename, setCourseName] = useState("Web");
+  const [instructor, setInstructor] = useState("Ramzi");
+  const [image, setImage] = useState("../assets/download.jpeg")
+  const [showAssignCourse, setShowAssignCourse] = useState(false);
   return(
+    <>
     <section className = 'section flex column'>
       <h1 className = 'title'>Courses</h1>
       <div className = 'allcourses flex'>
-        <Course/>
-        <Course/>
-        <Course/>
-        <Course/>
-        <Course/>
+        <Course
+        onClick = {() => {
+          showAssignPopup();
+        }}
+         />
+
 
 
       </div>
     </section>
+    <AssignCourses
+    open = {showAssignCourse}
+    coursename = {coursename}
+    onClose = {() => {
+      closeAssignPopup();
+    }}
+    />
+    </>
   );
 };
 export default AdminCourses;
