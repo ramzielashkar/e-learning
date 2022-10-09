479f7f652849902243f23143e88fedfcab51c4e5
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 18:17:14 +0300

    Updating All Courses Component

--- e-learning_frontend/src/components/AdminCourses.js
+++ e-learning_frontend/src/components/AdminCourses.js
@@ -1,8 +1,16 @@
+import Course from './Course';
 const AdminCourses = () =>{
   return(
     <section className = 'section flex column'>
       <h1 className = 'title'>Courses</h1>
       <div className = 'allcourses flex'>
+        <Course/>
+        <Course/>
+        <Course/>
+        <Course/>
+        <Course/>
+
+
       </div>
     </section>
   );
