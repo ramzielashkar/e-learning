66883a12ccf3631a6b188385b8c63e8d0141d5ed
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 17:40:35 +0300

    Creating All Courses Component

---
+++ e-learning_frontend/src/components/AdminCourses.js
@@ -0,0 +1,10 @@
+const AdminCourses = () =>{
+  return(
+    <section className = 'section flex column'>
+      <h1 className = 'title'>Courses</h1>
+      <div className = 'allcourses flex'>
+      </div>
+    </section>
+  );
+};
+export default AdminCourses;
