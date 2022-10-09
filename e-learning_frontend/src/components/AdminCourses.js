27ce981affa1441d307e6087fdc51ddc25ae3150
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 21:55:23 +0300

    Updating All Courses Component

--- e-learning_frontend/src/components/AdminCourses.js
+++ e-learning_frontend/src/components/AdminCourses.js
@@ -1,22 +1,41 @@
 import Course from './Course';
 import AssignCourses from './AssignCourses';
+import AddCourse from './AddCourse';
+import Button from './Button';
 import { useState, useEffect } from "react";
 
 const AdminCourses = () =>{
   const showAssignPopup = () => {
     setShowAssignCourse(true);
   };
-const closeAssignPopup = () => {
-  setShowAssignCourse(false);
-};
+  const closeAssignPopup = () => {
+    setShowAssignCourse(false);
+  };
+  const showAddPopup = () => {
+    setShowAddCourse(true);
+  };
+  const closeAddPopup = () => {
+    setShowAddCourse(false);
+  };
 
   const [coursename, setCourseName] = useState("Web");
   const [instructor, setInstructor] = useState("Ramzi");
   const [image, setImage] = useState("../assets/download.jpeg")
   const [showAssignCourse, setShowAssignCourse] = useState(false);
+  const [showAddCourse, setShowAddCourse] = useState(false);
+
   return(
     <>
     <section className = 'section flex column'>
+      <div className='add-course-btn flex'>
+        <Button
+        text = {'Add Course'}
+        color = {'btn-purple'}
+        onClick = {() => {
+          showAddPopup();
+        }}
+        />
+      </div>
       <h1 className = 'title'>Courses</h1>
       <div className = 'allcourses flex'>
         <Course
@@ -34,6 +53,12 @@ const closeAssignPopup = () => {
     coursename = {coursename}
     onClose = {() => {
       closeAssignPopup();
+    }}
+    />
+    <AddCourse
+    open = {showAddCourse}
+    onClose = {() => {
+      closeAddPopup();
     }}
     />
     </>
