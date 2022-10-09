c8a84e8d06461e827b8d56982c81820c8fc66233
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 11:48:08 +0300

    Frontend: Creating Top Bar Component

---
+++ e-learning_frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+
+const Header = () => {
+  return(
+  <div className = "top-bar flex">
+    <h1 className = "title">Learn Your Way</h1>
+    <div className='profile flex'>
+    <div className='user-img'>
+    <img src = {require('../assets/default.png')} width = '100%' height = '100%'></img>
+    </div>
+    <h3 className = "user-name">Ramzi El Ashkar</h3>
+    </div>
+  </div>
+);
+};
+export default Header;
