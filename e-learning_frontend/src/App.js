e3d32739423a80b9bac6fc3ec5cde611a4f7e1df
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 23:10:31 +0300

    Updating Routes

--- e-learning_frontend/src/App.js
+++ e-learning_frontend/src/App.js
@@ -1,23 +1,49 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import AdminSidebar from "./components/AdminSidebar";
+import Login from "./components/Login";
+import Admin from "./components/Admin";
+import AdminCourses from "./components/AdminCourses";
+import Students from "./components/Students";
+import Instructors from "./components/Instructors";
+
+
+const showLogin = (user) => {
+  console.log('email', user.email);
+  console.log('password', user.password);
+
+}
+const add = () => {
+  console.log();
+};
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+    <Header /
+    >
     <Routes>
     <Route path="/" element={
       <div>
+        <Login onAdd={showLogin}/>
+      </div>
+    } />
 
-      </div>
-    } />
     <Route path="/admin" element={
-      <div>
-        <Sidebar />
-      </div>
-    } />
+      <Admin />
+    } >
+      <Route path = 'allcourses' element={
+        <AdminCourses />
+      } />
+      <Route path = 'students' element={
+        <Students />
+      } />
+      <Route path = 'instructors' element={
+        <Instructors />
+      } />
+    </Route>
+
 
     </Routes>
     </BrowserRouter>
