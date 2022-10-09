19ece714fa2b83523675daa586c7241779c7ef99
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-09 18:18:18 +0300

    Updating Nav Item Component

--- e-learning_frontend/src/components/Navitem.js
+++ e-learning_frontend/src/components/Navitem.js
@@ -1,12 +1,16 @@
 import { FaBook } from 'react-icons/fa';
 import { FaUserGraduate } from 'react-icons/fa';
 import { FaUserTie } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+
 
 const Navitem = ({text}) =>{
   return (
+    <Link to = {"allcourses"} className='link'>
       <div className='nav-item flex'>
         <p>{text}</p>
       </div>
+      </Link>
     );
 };
 
