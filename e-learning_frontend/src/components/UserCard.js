f2f8c002e1596812602c7d9a554653af9bcfe459
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-10 23:36:07 +0300

    Frontend: Updating User Card Component

--- e-learning_frontend/src/components/UserCard.js
+++ e-learning_frontend/src/components/UserCard.js
@@ -1,14 +1,14 @@
 import Button from './Button';
 
-const UserCard = (onClick) => {
+const UserCard = ({onClick, name, email}) => {
   return(
     <>
     <div className = "course-card flex column">
       <div className='course-img'>
         <img src = {require('../assets/images.jpeg')} width = "100%" height = "100%"></img>
       </div>
-      <div className='user-info'>Ramzi</div>
-      <div className='user-info'>Email</div>
+      <div className='user-info'>{name}</div>
+      <div className='user-info'>{email}</div>
     </div>
     </>
   );
