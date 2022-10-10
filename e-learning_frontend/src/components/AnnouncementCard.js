c86c79c4d3013b34e2ad230b24450721ac15e83a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-10 15:38:21 +0300

    Adding Instructor Announcement Card Component

---
+++ e-learning_frontend/src/components/AnnouncementCard.js
@@ -0,0 +1,16 @@
+
+const AnnouncementCard = () => {
+  return(
+    <div className='assignment-card flex column'>
+      <div className='assignment-info '>
+        <div className='assignment'>Announcement</div>
+        <div>Create a web app</div>
+      </div>
+      <div className='assignment-info '>
+        <div className='assignment'>Course</div>
+        <div> Web Programming</div>
+      </div>
+    </div>
+  );
+};
+export default AnnouncementCard;
