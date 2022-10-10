ab3dbfa6f7d111319916e24000d1724d0324a7c1
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-10 15:39:31 +0300

    Updating Assign Courses Popup

--- e-learning_frontend/src/components/AssignCourses.js
+++ e-learning_frontend/src/components/AssignCourses.js
@@ -1,19 +1,19 @@
 import Button from './Button';
 
-const AssignCourses = ({open, image, coursename, onClose}) => {
+const AssignCourses = ({open, text, coursename, onClose}) => {
   if(!open){
     return null;
   }
 
   return(
     <div className='assign-container flex column'>
-      <div className='assign-popup flex column'>
+      <form className='assign-popup flex column'>
         <div className='course-info flex column'>
           <label>Course Name</label>
           <input value={coursename}></input>
         </div>
         <div className = 'instructor-info flex column'>
-        <label>Instructor</label>
+        <label>{text}</label>
         <select name="instructor">
           <option value="1">Ramzi</option>
           <option value="2">Karam</option>
@@ -25,12 +25,9 @@ const AssignCourses = ({open, image, coursename, onClose}) => {
         text= {'Close'}
         color={'btn-assign'}
         onClick = {onClose}/>
-        <Button
-        text= {'Assign'}
-        color={'btn-assign'}
-        onClick = {onClose}/>
+        <input type={"submit"} value="Assign" className="btn btn-assign" />
         </div>
-      </div>
+      </form>
     </div>
   );
 };
