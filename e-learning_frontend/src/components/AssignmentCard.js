0f21818e3603f09d1231c1e953eacdb7289cdb7a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-10 14:58:37 +0300

    Adding Assignments Card Component

---
+++ e-learning_frontend/src/components/AssignmentCard.js
@@ -0,0 +1,21 @@
+
+
+const AssignmentCard = () => {
+  return(
+    <div className='assignment-card flex column'>
+      <div className='assignment-info'>
+        <div className='assignment'>Assignment Name</div>
+        <div> Assignment 1</div>
+      </div>
+      <div className='assignment-info '>
+        <div className='assignment'>Desription</div>
+        <div>Create a web app</div>
+      </div>
+      <div className='assignment-info '>
+        <div className='assignment'>Course</div>
+        <div> Web Programming</div>
+      </div>
+    </div>
+  );
+};
+export default AssignmentCard;
