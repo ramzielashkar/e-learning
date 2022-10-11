9e63b5c7b235d9afc0d6a3c98d1aa315855d8b2c
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-11 12:39:25 +0300

    Frontend: Updating get Courses API

--- e-learning_frontend/src/Hooks/getAllCourses.js
+++ e-learning_frontend/src/Hooks/getAllCourses.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-export const getAllCourses = async (token) => {
-    const res = await axios.get("http://127.0.0.1:8000/api/v0.1/getCourses",{
+export const getAllCourses = async (token, assigned='') => {
+    const res = await axios.get(`http://127.0.0.1:8000/api/v0.1/getCourses/${assigned}`,{
         headers: {
           "Content-type": "application/json",
           'Authorization' : "Bearer" + token
