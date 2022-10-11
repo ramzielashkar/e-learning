51d40d2a28b2b222366e72d565c87bddedadca3e
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-11 22:30:16 +0300

    Frontend: Updating Add Announcement Popup

--- e-learning_frontend/src/components/AddAnnouncement.js
+++ e-learning_frontend/src/components/AddAnnouncement.js
@@ -1,25 +1,56 @@
 import Button from './Button';
+import { getCourses } from '../Hooks/getInstructorCourses';
+import { useState, useEffect } from 'react';
 
-const AddAnnouncement = ({open, text, coursename, onClose}) => {
+const AddAnnouncement = ({open, text, coursename, onClose, onAdd}) => {
+  const token = localStorage.getItem('token');
+  const [courses, setCourses] = useState([]);
+  const [course, setCourse] = useState('');
+  const [announcement, setAnnouncement] = useState('');
+
+  //function to add Asnnouncement
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!course) {
+    console.log('error');
+    return;
+    } 
+    onAdd(course, announcement);
+    onClose();
+} 
+
+  //fetching Instructor courses
+  useEffect(() => {
+    const getInstructorCourses = async (token) => {
+      const coursesFromServer = await getCourses(token);
+      setCourses(coursesFromServer.data.courses);
+      setCourse(coursesFromServer.data.courses['0'].name);
+    };
+    getInstructorCourses(token);
+  }, []);
+
   if(!open){
     return null;
   }
   
   return(
     <div className='assign-container flex column'>
-      <form className='assign-popup flex column'>
+      <form className='assign-popup flex column' onSubmit={onSubmit}>
         <div className='course-info flex column'>
           <label>Announcement </label>
-          <textarea className='assg-details' placeholder="Enter Announcement" rows="10"></textarea>
+          <textarea className='assg-details' placeholder="Enter Announcement" rows="10" onChange={(e) => {
+          setAnnouncement(e.target.value)}}></textarea>
         </div>
         <div className = 'instructor-info flex column'>
         <label>Course</label>
-        <select name="course">
-          <option value="1">Web Programming</option>
-          <option value="2">Mobile App</option>
-          <option value="3">Intro</option>
+        <select name="course" value='' onChange={(e) => {
+          setCourse(e.target.value)}}>
+            {courses.map((course, index) =>{
+            return(
+              <option value={course.name}>{course.name}</option>
+            );
+          })};
         </select>
-
         </div>
         <div className='buttons flex'>
         <Button
