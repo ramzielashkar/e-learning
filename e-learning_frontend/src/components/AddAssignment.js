f69225dbebf297d07f61d90c3b731fe5835ddb33
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-11 21:54:20 +0300

    Frontend: Updating Add Assignment Popup

--- e-learning_frontend/src/components/AddAssignment.js
+++ e-learning_frontend/src/components/AddAssignment.js
@@ -1,27 +1,60 @@
+import { useState, useEffect } from 'react';
 import Button from './Button';
+import { getCourses } from '../Hooks/getInstructorCourses';
 
-const AddAssignment = ({open, text, coursename, onClose}) => {
+const AddAssignment = ({open, text, coursename, onClose, onAdd}) => {
+  const [course, setCourse] = useState('');
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+
+  //function to add Assignment
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!course) {
+    console.log('error');
+    return;
+    } 
+    onAdd(course, name, details);
+    onClose();
+} 
+  const token = localStorage.getItem('token');
+  const [courses, setCourses] = useState([]);
+  
+  useEffect(() => {
+    const getInstructorCourses = async (token) => {
+      const coursesFromServer = await getCourses(token);
+      setCourses(coursesFromServer.data.courses);
+      //console.log(coursesFromServer.data.courses['0'].name);
+      setCourse(coursesFromServer.data.courses['0'].name);
+    };
+    getInstructorCourses(token);
+  }, []);
   if(!open){
     return null;
   }
 
   return(
     <div className='assign-container flex column'>
-      <form className='assign-popup flex column'>
+      <form className='assign-popup flex column' onSubmit={onSubmit}>
         <div className='course-info flex column'>
           <label>Assignment Name</label>
-          <input type = 'text'></input>
+          <input type = 'text' onChange={(e) => {
+          setName(e.target.value)}}></input>
         </div>
         <div className='course-info flex column'>
           <label>Assignment Description</label>
-          <textarea className='assg-details' placeholder="Enter Assignment Details" rows="10"></textarea>
+          <textarea className='assg-details' placeholder="Enter Assignment Details" rows="10" onChange={(e) => {
+          setDetails(e.target.value)}}></textarea>
         </div>
         <div className = 'instructor-info flex column'>
         <label>Course</label>
-        <select name="course">
-          <option value="1">Web Programming</option>
-          <option value="2">Mobile App</option>
-          <option value="3">Intro</option>
+        <select name="course" value='' onChange={(e) => {
+          setCourse(e.target.value)}}>
+            {courses.map((course, index) =>{
+            return(
+              <option value={course.name}>{course.name}</option>
+            );
+          })};
         </select>
 
         </div>
