27f07570363e74328c076aff687b0684ae69c3b7
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-11 12:52:17 +0300

    Frontend: Creating a Hook to access add User API

---
+++ e-learning_frontend/src/Hooks/AddUser.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+export const addUser = async (data, token) => {
+    const res = await axios.post("http://127.0.0.1:8000/api/v0.1/addUser", data,{
+        headers: {
+          "Content-type": "application/json",
+          'Authorization' : "Bearer" + token
+        },
+      });
+      return res;
+ }
