1d3dc016f6538981bced6ed050eaa1846f4a89a5
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-11 12:38:00 +0300

    Frontend: Updating Assign Courses Popup

--- e-learning_frontend/src/components/AssignCourses.js
+++ e-learning_frontend/src/components/AssignCourses.js
@@ -1,23 +1,50 @@
 import Button from './Button';
+import { getInstructors } from '../Hooks/getInstructors';
+import { useState, useEffect } from 'react';
+import { assignCourse } from '../Hooks/AssignCourses';
+
 
 const AssignCourses = ({open, text, coursename, onClose}) => {
+  //const [course, setCourseName] = useState('');
+  const [instructor, setInstructor] = useState('');
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const token =localStorage.getItem('token');
+    console.log(instructor);
+    const res = await assignCourse({coursename, instructor}, token);
+    onClose();
+} 
+  const token = localStorage.getItem('token');
+  const [instructors, setInstructors] = useState([]);
+  
+  useEffect(() => {
+    const getAllInstructors = async (token) => {
+      const instructorsFromServer = await getInstructors(token);
+      setInstructors(instructorsFromServer.data.users);
+    };
+    getAllInstructors(token);
+  }, []);
+
   if(!open){
     return null;
   }
 
   return(
     <div className='assign-container flex column'>
-      <form className='assign-popup flex column'>
+      <form className='assign-popup flex column' onSubmit={onSubmit}>
         <div className='course-info flex column'>
           <label>Course Name</label>
           <input value={coursename}></input>
         </div>
         <div className = 'instructor-info flex column'>
         <label>{text}</label>
-        <select name="instructor">
-          <option value="1">Ramzi</option>
-          <option value="2">Karam</option>
-          <option value="3">Marwan</option>
+        <select name="instructor" value ={instructor} onChange={(e) => {
+          setInstructor(e.target.value)}}>
+          {instructors.map((instructor, index) =>{
+            return(
+              <option value={instructor.name}>{instructor.name}</option>
+            );
+          })};
         </select>
         </div>
         <div className='buttons flex'>
